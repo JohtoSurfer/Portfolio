410d956d80aa5fe007e47662d9e06fbeba3b7742
Luis Barrios <104099952+JohtoSurfer@users.noreply.github.com>
2025-06-02 15:54:29 -0400

    type: fix summary: bug with boostrap installation fix, some files modified like resources.

--- Laravel/pokedex/resources/js/app.js
+++ Laravel/pokedex/resources/js/app.js
@@ -1,7 +1,9 @@
-import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa CSS de Bootstrap
+import 'bootstrap'; // Importa JS de Bootstrap (modales, dropdowns, etc.)
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
