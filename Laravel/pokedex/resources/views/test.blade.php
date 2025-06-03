ec6c0d78669a2100e2601c10717e9e20d09c9b84
Luis Barrios <104099952+JohtoSurfer@users.noreply.github.com>
2025-06-03 19:11:46 -0400

    type: test summary: something is not working properly, the main laravel web is loading everything big and out of place, it need fixing.

---
+++ Laravel/pokedex/resources/views/test.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-center text-primary">It Works!</h1>
+    <button class="btn btn-success">Test Bootstrap Button</button>
+@endsection
